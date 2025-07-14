989653278a0d4004ea488c3f195d5171a8680650
Frishhhdesu <irishfermin910@gmail.com>
2025-07-14 17:18:55 +0800

    updateeee

--- profile.php
+++ profile.php
@@ -4,14 +4,14 @@ require 'header.php';
 
 <!--Profile-->
         <div class="p-4 mt-1 row">
-            <h2 class="fw-bold fs-1" style="color: #d9d9d9; text-shadow: 2px 2px 5px #727272;">Profile</h2>
+            <h2 class="fw-bold fs-1" style="color: #d9d9d9;">Profile</h2>
         </div>
         <div class="row p-1 ms-4">
             <div class="col-lg-3 ms-4">
                 <form>
                     <label for="file">
                         <div class="profile-pic shadow" id="set-pic" style="background-image: url('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png')">
-                            <span style="margin-top: -20%;"><i class="bi bi-camera fs-1"></i></span>
+                            <span style="margin-top:;"><i class="bi bi-camera fs-1"></i></span>
                         </div>
                     </label>
                     <input type="file" name="file" id="file" accept="image/*" onchange="loadFile(event)">
@@ -19,13 +19,6 @@ require 'header.php';
             </div>
             <div class="col-md-5 fw-bold fs-4 mt-3 ms-5 me-3 username-fade">
                 <strong class="me-3">User ID:</strong><span id="playerID"></span><br>
-            </div>
-            <div class="col ms-1">
-                <div style="position: relative; display: inline-block;" class="viewOrder">
-                    <a href="/pages/Past_Orders/orders.html" class="btn rounded-pill bg-primary text-light fs-5 fw-bold px-4 py-3 mt-3 shadow"> View All Orders</a>
-                    <img src="../../assets/Pikachu.png" alt="" style="position: absolute; height: 85%; top: -40%; left: 65%;"></img>
-                </div>
-                
             </div>
         </div>
         <div class="row p-4 mt-1 ms-4 fw-bold fs-4 pb-5">
@@ -38,37 +31,46 @@ require 'header.php';
             <div class="col me-5">
                 <div class="row">
                     <div class="col">
-                        <label style="color: #d9d9d9; text-shadow: 2px 2px 5px #5c5c5c;">User Name</label><br>
+                        <label style="color: #d9d9d9;">User Name</label><br>
                         <input type="text" id="name" class="form-control" placeholder="Name" required style="width: 82%;"><br>
                     </div>
                     <div class="col">
-                        <label class="fw-bold" style="color: #d9d9d9; text-shadow: 2px 2px 5px #5c5c5c;">Password</label>
+                        <label class="fw-bold" style="color: #d9d9d9;">Password</label>
                         <div class="input-group" style="width: 75%;">
                             <input type="password" class="form-control" id="password" placeholder="Password" required>
-                            <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                            <button class="btn btn-outline-light" type="button" id="togglePassword">
                                 <i class="bi bi-eye"></i>
                             </button>
                         </div>
                     </div>
                 </div>
-            <form id="formValidate">
-                <div class="row">
-                    <div class="col">
-                        <label style="color: #d9d9d9; text-shadow: 2px 2px 5px #5c5c5c;">Email Address</label><br>
-                        <input type="email" class="form-control" id="email" placeholder="Email Address" required style="width: 40%;">
-                    </div>
-                </div>                    
+                <form id="formValidate">
+                    <div class="row">
+                        <div class="col">
+                            <label style="color: #d9d9d9;">Email Address</label><br>
+                            <input type="email" class="form-control" id="email" placeholder="Email Address" required style = "width: 82%">
+                        </div>
+                        <div class="col">
+                            <label class="fw-bold" style="color: #d9d9d9;">Delivery Address</label>
+                            <div class="input-group" style="width: 75%;">
+                            <input type="password" class="form-control" id="password" placeholder="Address" required>
+                            <button class="btn btn-outline-light" type="button" id="togglePassword">
+                                Edit
+                            </button>
+                        </div>
+                        </div>
+                    </div>                    
             </div>
 
             <!--Log out and Save button-->
-            <div class="row" style="margin-top: 5%;">         
-                <div class="col mt-3">
-                    <a href="/pages/Sign_In_Up/signin.html" id="logout-btn" class="text-danger profile-link" ><i class="bi bi-power me-3"></i>Log Out</a>
-                </div>    
-                <div class="col offset-lg-6">
-                    <button type="submit" class="btn ms-3 rounded-pill bg-primary text-light fs-5 px-4 py-3 fw-bold shadow-lg viewOrder" onclick="saveUserData()"><i class="bi bi-floppy me-2"></i> Save Settings</button>
-                </div>       
-            </div>
+                <div class="row" style="margin-top: 5%;">         
+                    <div class="col mt-3">
+                        <a href="/pages/Sign_In_Up/signin.html" id="logout-btn" class="text-primary profile-link" ><i class="bi bi-power me-3"></i>Log Out</a>
+                    </div>    
+                    <div class="col offset-lg-6">
+                        <button type="submit" class="btn ms-3 rounded-pill bg-primary text-light fs-5 px-4 py-3 fw-bold shadow-lg viewOrder" onclick="saveUserData()"><i class="bi bi-floppy me-2"></i> Save Settings</button>
+                    </div>       
+                </div>
             </form>
         </div>  
 

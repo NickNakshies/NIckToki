adacf268a469827c3627efd97dfc274dff715167
JustineClaro <nicknakshies@gmail.com>
2025-07-14 15:33:22 +0800

    commit langch

---
+++ store.php
@@ -0,0 +1,113 @@
+        <!-- SEPERATE TS -->
+        <div class="label-name-1" >
+            <h1>Item Boxes</h1>
+        </div>
+            
+       <!-- ITEM BOXES -->
+
+        <section class="item-boxes" id="item-boxes">
+
+            <!--  -->
+            <div class="card" id="item-1" >
+                <img class="main-image" src="../../assets/Bug Out Ultra Ticket Box.png" alt="Main Item Box">
+                <div class="small-images">
+                    <img src="../../assets/Ticket (2).png" alt="Small Item 1">
+                    <span>1</span>
+                    <p class="hidden-name">Ticket</p>
+                    <img src="../../assets/Rare Candy.png" alt="Small Item 2">
+                    <p class="hidden-name">Rare Candy</p>
+                    <span>2</span>
+                </div>
+                <div class="card-content">
+                    <div>
+                        <h3>Bug Out Ultra Ticket Box</h3>
+                        <p>Can only be purchased 1 time</p>
+                    </div>
+                    <button class="price-button">₱ <span>59</span></button>
+                </div>
+            </div>
+   
+<!--  -->
+            <div class="card scroll-offset" id="item-2">
+                <img class="main-image" src="../../assets/GO Rocket Box.png" alt="Main Item Box">
+                <div class="small-images">
+                    <img src="../../assets/Ultra Ball.png" alt="Small Item 1">
+                    <span>7</span>
+                    <p class="hidden-name">Ultra Ball</p>
+                    <img src="../../assets/Rocket Radar.png" alt="Small Item 2">
+                    <span>7</span>
+                    <p class="hidden-name">Rocket Radar</p>
+                    <img src="../../assets/Premium Battle Pass.png" alt="Small Item 3">
+                    <span>7</span>
+                    <p class="hidden-name">Premium Battle Pass</p>
+                    <img src="../../assets/Silver Sinap Berry.png" alt="Small Item 4">
+                    <span>7</span>
+                    <p class="hidden-name">Silver Sinap Berry</p>
+                </div>
+                <div class="card-content">
+                    <div>
+                        <h3>GO Rocket Box</h3>
+                        <p>Can only be purchased 1 time</p>
+                    </div>
+                    <button class="price-button">₱ <span>289</span></button>
+                </div>
+            </div>
+
+            <div class="card" id="item-3">
+                <img class="main-image" src="../../assets/Ultra Raid Box.png" alt="Main Item Box">
+                <div class="small-images">
+                    <img src="../../assets/Remote Raid Pass.png" alt="Small Item 1">
+                    <span>3</span>
+                    <p class="hidden-name">Remote Raid Pass</p>
+                    <img src="../../assets/Premium Battle Pass.png" alt="Small Item 2">
+                    <span>3</span>
+                    <p class="hidden-name">Premium Battle Pass</p>
+                    <img src="../../assets/Egg Incubator.png" alt="Small Item 3">
+                    <span>1</span>
+                    <p class="hidden-name">Egg Incubator</p>
+                </div>
+                <div class="card-content">
+                    <div>
+                        <h3>Ultra Raid Box</h3>
+                        <p>Can only be purchased 3 times</p>
+                    </div>
+                    <button class="price-button">₱ <span>149</span></button>
+                </div>
+            </div>
+
+            <div class="card" id="item-4">
+                <img class="main-image" src="../../assets/Might and Mastery Box.png" alt="Main Item Box">
+                <div class="small-images">
+                    <img src="../../assets/Postcard Pages.png" alt="Small Item 1">
+                    <span>1</span>
+                    <p class="hidden-name">Postcard Pages</p>
+                    <img src="../../assets/Remote Raid Pass.png" alt="Small Item 2">
+                    <span>3</span>
+                    <p class="hidden-name">Remote Raid Pass</p>
+                    <img src="../../assets/Premium Battle Pass.png" alt="Small Item 3">
+                    <span>2</span>
+                    <p class="hidden-name">Premium Battle Pass</p>
+                </div>
+                <div class="card-content">
+                    <div>
+                        <h3>Might and Mastery Box</h3>
+                        <p>Can only be purchased 3 times</p>
+                    </div>
+                    <button class="price-button">₱ <span>149</span></button>
+                </div>
+            </div>
+            <div class="card" id="item-5">
+                <img class="main-image" src="../../assets/Super Incubator.png" alt="Main Item Box">
+                <div class="small-images">
+                    <img src="../../assets/Super Incubator.png" alt="Small Item 1">
+                    <span>2</span>
+                    <p class="hidden-name">Super Incubator</p>
+                </div>
+                <div class="card-content">
+                    <div>
+                        <h3>Super Incubator</h3>
+                        <p><br></p>
+                        <br>
+                    </div>
+                    <button class="price-button">₱ <span>88</span></button>
+                </div>

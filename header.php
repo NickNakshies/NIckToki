dce3f003d0b735b2a7fb5c74038f18cdef8df84a
Frishhhdesu <irishfermin910@gmail.com>
2025-07-14 13:41:48 +0800

    13:41 - Main template done

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/nicktokilogo.png" sizes="192x192" />
+    <title>NickToki</title>
+
+    <!-- BootStrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Styles natin -->
+     <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <!-- NAVBAR START -->
+    <nav class="navbar navbar-expand-lg navbar-dark px-2 ms-3" >
+        <a class="navbar-brand d-flex ms-5" href="#">
+            <img src="assets\nicktokilogo.png" alt="Logo" style="width: 70px; height: auto;" class="">
+            <h3 class="ms-3" style="margin-top: 13%;">NickToki.</h3>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+            aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar content -->
+        <div class="collapse navbar-collapse" id="navbarContent">
+
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto mt-0">
+                <li class="nav-item fs-5 me-3 nav-mt">
+                    <a class="nav-link fw-bold mt-2" href="#">Home</a>
+                </li>
+
+                <li class="nav-item fs-5 ms-3 me-3 nav-mt">
+                    <a class="nav-link mt-2">
+                        Gallery
+                    </a>
+                </li>
+
+                <li class="nav-item fs-5 ms-3 me-3 nav-mt">
+                    <a class="nav-link mt-2" href="#footer">About</a>
+                </li>
+                <li class="nav-ite mt-5">
+                    <a class="nav-link mt-2" href="/pages/Cart/cart.html">
+                        <img src="assets/searchbtn.png" style="width: auto; height: 10vh;">
+                    </a>
+                </li>
+                <li class="nav-item mt-5">
+                    <a class="nav-link mt-2" href="/pages/Cart/cart.html">
+                        <img src="assets/cartbtn.png" style="width: auto; height: 10vh;">
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../pages/Profile/profile.html">
+                        <img src="assets/signupbtn.png" style="width: auto; height: 26vh;"/>
+                    </a>   
+                </li>
+            </ul>
+        </div>
+    </nav>  
+     
+    <div class="main-window">
+        
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require 'header.php';
+?>
+
+<!-- Content here -->
+
+<?php
+require 'footer.php';
+?>
